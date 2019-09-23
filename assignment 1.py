6adfa9bfec7980b7a6cd80e286b36f8d3b691de8
antara-gaikwad <54177051+antara-gaikwad@users.noreply.github.com>
2019-09-23 12:08:20 +0530

    this is basic assignment on importing libraries.

---
+++ assignment 1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#importing libraries:
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+# print the name :
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
